af27337d6ad171206a8865beb1e362a2be68e6c4
Sakthi priya M <sanjayram9486@gmail.com>
2026-04-21 07:27:39 +0530

    Create test_calculator.py

---
+++ test_calculator.py
@@ -0,0 +1,17 @@
+from src.calculator import add, subtract, multiply, divide
+
+def test_add():
+    assert add(2, 3) == 5
+
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+
+def test_multiply():
+    assert multiply(2, 4) == 8
+
+def test_divide():
+    assert divide(10, 2) == 5
+
+def test_divide_by_zero():
+    assert divide(10, 0) == "Cannot divide by zero"
